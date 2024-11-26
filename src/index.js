8c09f07511f2fca2aa9ed56e6397345e2b8c7555
Allan <38442183+allan-alb@users.noreply.github.com>
2024-11-26 13:53:39 -0300

    fix: add error handler, save data to file

--- src/index.js
+++ src/index.js
@@ -5,14 +5,52 @@ import {SerialPort, ReadlineParser} from 'serialport'
 import ejs from 'ejs'
 import path from 'path'
 import {fileURLToPath} from 'url'
+import fs from 'fs/promises'
 
 const app = express()
 const httpServer = http.createServer(app)
 const io = new Server(httpServer)
 
+const saveDataToFile = async (data) => {
+	let file = 'C:/Users/user/Desktop/monitorfiles'
+
+	if (data.includes('Temperature')) {
+		file = file + '/temperature.txt'
+	}
+	
+	if (data.includes('Light')) {
+		file = file + '/light.txt'
+	}
+	
+	if (data.includes('Moisture')) {
+		file = file + '/moisture.txt'
+	}
+	
+	const content = data + " (" + new Date().toLocaleString('pt') + ")\n"
+	
+	try {
+		await fs.appendFile(file, content);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+const saveErrorsToFile = async (errorData) => {
+	const errorFile = 'C:/Users/user/Desktop/monitorfiles/error.txt'
+	
+	const content = errorData + " (" + new Date().toLocaleString('pt') + ")\n"
+	
+	try {
+		await fs.appendFile(file, content);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
 const serialPort = new SerialPort({
-  path: '/dev/ttyACM0',
-  baudRate: 9600
+  path: 'COM3',
+  baudRate: 9600,
+  autoOpen: true
 })
 
 const serialParser = serialPort.pipe(new ReadlineParser({ delimiter: '\r\n' }))
@@ -32,11 +70,28 @@ app.get('/', (req, res) => {
 serialPort.on('open', () => {
   console.log('serial port opened')
 })
+serialPort.on('close', (e) => {
+  console.log('serial port closed at ', new Date().toLocaleString('pt'))
+  console.log('event data: ', JSON.stringify(e))
+  saveErrorsToFile('port closed ' + JSON.stringify(e))
+  
+  serialPort.open((err) => {
+	  if (err) {
+		saveErrorsToFile('error reopening: ' + err.message)
+		return console.log('error reopening port: ', err.message)
+	  }
+  })
+})
+serialParser.on('data', (data) => {
+    console.log(data)
+	saveDataToFile(data)
+})
 io.on('connection', (socket) => {
   console.log('socket.io connection')
   serialParser.on('data', (data) => {
     console.log(data)
     socket.emit('arduino-data', data)
+	// saveDataToFile(data)
   })
   socket.on('disconnect', () => {
     console.log('disconnected')
